f7a23dfc87b7e941e34dd7f4bf1590534549ccda
Jakub Gawryl <jacqbus15@gmail.com>
2021-03-18 22:03:29 +0100

    Remove example + update build

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,21 @@
 const {src, dest, series} = require('gulp')
 const ts = require('gulp-typescript')
 const del = require('del')
+const merge = require('merge2');
 
 const targetDir = './dist'
-const project = ts.createProject('tsconfig.json')
+const project = ts.createProject('tsconfig.json', {
+  declaration: true
+})
 
 const clean = () => del([targetDir])
 
 const parseTS = () => {
   const tsResult = src("src/**/*.ts").pipe(project())
-  return tsResult.js.pipe(dest(targetDir))
+  return merge(
+    tsResult.dts.pipe(dest(targetDir)),
+    tsResult.js.pipe(dest(targetDir))
+  )
 }
 
 const copyFiles = () => src("src/**/*.*", {ignore: "src/**/*.ts"}).pipe(dest(targetDir));
